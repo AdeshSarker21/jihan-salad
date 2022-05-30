822f9bb5f14cc98a092ed25b873d781dceebefbd
AdeshSarker21 <adeshchandrosarker.sdp@gmail.com>
2022-05-30 23:05:12 +0600

    First Commit

---
+++ assets/js/custom.js
@@ -0,0 +1,96 @@
+/*project item one*/
+$(document).ready(function(){
+  $(".toggle_one").click(function(){
+    $(".details_one").toggle();
+  });
+     $(".cross_one").click(function(){
+    $(".details_one").hide();
+  });
+     $(".toggle_one").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_one").click(function(){
+    $(".wapper_effect").hide();
+  });
+    /*project item two*/
+     $(".toggle_two").click(function(){
+    $(".details_two").toggle();
+  });
+     $(".cross_two").click(function(){
+    $(".details_two").hide();
+  });
+     $(".toggle_two").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_two").click(function(){
+    $(".wapper_effect").hide();
+  });
+     /*project item three*/
+     $(".toggle_three").click(function(){
+    $(".details_three").toggle();
+  });
+     $(".cross_three").click(function(){
+    $(".details_three").hide();
+  });
+     $(".toggle_three").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_three").click(function(){
+    $(".wapper_effect").hide();
+  });
+         /*project item four*/
+     $(".toggle_four").click(function(){
+    $(".details_four").toggle();
+  });
+     $(".cross_four").click(function(){
+    $(".details_four").hide();
+  });
+     $(".toggle_four").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_four").click(function(){
+    $(".wapper_effect").hide();
+  });
+             /*project item five*/
+     $(".toggle_five").click(function(){
+    $(".details_five").toggle();
+  });
+     $(".cross_five").click(function(){
+    $(".details_five").hide();
+  });
+     $(".toggle_five").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_five").click(function(){
+    $(".wapper_effect").hide();
+  });
+                 /*project item six*/
+     $(".toggle_six").click(function(){
+    $(".details_six").toggle();
+  });
+     $(".cross_six").click(function(){
+    $(".details_six").hide();
+  });
+     $(".toggle_six").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_six").click(function(){
+    $(".wapper_effect").hide();
+  });
+    
+                     /*click*/
+     $(".click").click(function(){
+    $(".details_one").toggle();
+  });
+     $(".cross_six").click(function(){
+    $(".details_six").hide();
+  });
+     $(".click").click(function(){
+    $(".wapper_effect").toggle();
+  });
+      $(".cross_six").click(function(){
+    $(".wapper_effect").hide();
+  });
+});
+
+
